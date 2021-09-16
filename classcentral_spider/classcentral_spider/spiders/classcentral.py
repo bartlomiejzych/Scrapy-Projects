e895b2bfffeac42178ecbe59e0d825db6909207f
Bartłomiej Zych <bartlomiejzych98@gmail.com>
2021-09-16 22:24:53 +0200

    Corrected a few errors

--- classcentral_spider/classcentral_spider/spiders/classcentral.py
+++ classcentral_spider/classcentral_spider/spiders/classcentral.py
@@ -5,14 +5,14 @@ from scrapy.http import Request
 class ClasscentralSpider(Spider):
     name = 'classcentral'
     allowed_domains = ['classcentral.com']
-    start_urls = ['https://www.classcentral.com/subjects/']
+    start_urls = ['https://www.classcentral.com/subjects']
 
     def __init__(self, subject=None):
         self.subject = subject
 
     def parse(self, response):
         if self.subject:
-            subject_url = response.xpath('//a[contains(@title, "Programming")]/@href').extract_first()
+            subject_url = response.xpath('//a[contains(@title, "' + self.subject + '")]/@href').extract_first()
             absolute_subject_url = response.urljoin(subject_url)
             yield Request(
                 absolute_subject_url,
