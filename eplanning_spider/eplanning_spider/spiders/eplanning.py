e70dc6d799e65d2c5e9dd8650cb5e4c0cf4ed5cc
Bartłomiej Zych <bartlomiejzych98@gmail.com>
2021-09-29 22:28:18 +0200

    Added function which uses FormRequest to fill forms

--- eplanning_spider/eplanning_spider/spiders/eplanning.py
+++ eplanning_spider/eplanning_spider/spiders/eplanning.py
@@ -1,5 +1,5 @@
 from scrapy import Spider
-from scrapy.http import Request
+from scrapy.http import Request, FormRequest
 
 class EplanningSpider(Spider):
     name = 'eplanning'
@@ -13,9 +13,26 @@ class EplanningSpider(Spider):
                 pass
             else:
                 yield Request(
-                    url,
-                    callback=self.parse_application,
-                    )
+                            url,
+                            callback=self.parse_application,
+                            )
 
     def parse_application(self, response):
+        app_url = response.xpath(
+            '//*[@class="glyphicon glyphicon-inbox btn-lg"]/following-sibling::a/@href').extract_first()
+        yield Request(
+                    response.urljoin(app_url),
+                    callback=self.parse_form,
+                    )
+
+    def parse_form(self, response):
+        yield FormRequest.from_response(
+                                    response,
+                                    formdata={'RdoTimeLimit': '42'},
+                                    dont_filter=True,
+                                    formxpath='(//form)[2]',
+                                    callback=self.parse_pages
+                                    )
+
+    def parse_pages(self, response):
         pass
