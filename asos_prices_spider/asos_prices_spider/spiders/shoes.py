aaf8f772b1cf560ab0cedc41ab1ed4ca1be04b71
Bartłomiej Zych <bartlomiejzych98@gmail.com>
2021-09-30 17:13:11 +0200

    Created spider which will scrape prices of products from asos.com

--- asos_prices_spider/asos_prices_spider/spiders/shoes.py
+++ asos_prices_spider/asos_prices_spider/spiders/shoes.py
@@ -4,7 +4,10 @@ import scrapy
 class ShoesSpider(scrapy.Spider):
     name = 'shoes'
     allowed_domains = ['asos.com']
-    start_urls = ['http://asos.com/']
+    start_urls = ['https://www.asos.com/men/shoes-boots-trainers/cat/?cid=4209&nlid=mw|shoes|shop+by+product|view+all']
 
     def parse(self, response):
-        pass
+        shoes_urls = response.xpath('//article[@data-auto-id="productTile"]/a/@href').extract()
+        for url in shoes_urls:
+            yield Request
+
